743597cc31b93998eb5863e7df0bf129afb7a8d3
Phillip Jackson <philwinkle@gmail.com>
2017-12-11 21:30:33 -0500

    Add AWS Mobile capability

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
 import App from './App';
 import './index.css';
+
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <App />,
