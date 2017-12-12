5f99ee6c8dcbdb2719b6868c89a48d5a4e444b4e
Phillip Jackson <philwinkle@gmail.com>
2017-12-12 00:13:07 -0500

    Add PWA service worker functionality

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Amplify from 'aws-amplify';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import aws_exports from './aws-exports';
 import './index.css';
 
-import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
